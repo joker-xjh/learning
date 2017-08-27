f99988488f2a4f8366f1baaed7b202a99c9c34bb
joker_xjh <1781934063@qq.com>
2017-08-27 14:53:45 +0800

    not complete........

--- net/src/main/java/demo11/Roll.java
+++ net/src/main/java/demo11/Roll.java
@@ -1,5 +1,24 @@
 package demo11;
 
-public class Roll {
+public class Roll implements Icommands{
+	
+	private static final String NAME = "Roll";
+	private static final String HELP = "/Roll - Returns a number between 0 and 99\"";
+
+	@Override
+	public String perform() {
+		int num = (int) (Math.random()*100);
+		return "Rolling (0-99): " + num + ".";
+	}
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public String help() {
+		return HELP;
+	}
 
 }
